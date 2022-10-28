87f998d9fce93304ed10fa659f7076fcad234aa3
OneSidedDie <41028043+OneSidedDie@users.noreply.github.com>
2022-10-27 20:20:06 -0400

    Update install.js

--- install.js
+++ install.js
@@ -1,4 +1,10 @@
 /** @param {NS} ns */
 export async function main(ns) {
+	while (ns.singularity.upgradeHomeRam()) {
+		await ns.sleep(buffer)
+	}
+	while (ns.singularity.upgradeHomeCores()) {
+		await ns.sleep(buffer);
+	}
 	ns.singularity.installAugmentations("nfg.js");
 }
