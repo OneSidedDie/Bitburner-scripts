7d4bef73d32c3fb459c8f9bcbce714d8c32887f9
OneSidedDie <41028043+OneSidedDie@users.noreply.github.com>
2024-10-04 10:05:11 -0400

    Update and rename functions.js to lib.js

--- lib.js
+++ lib.js
@@ -14,6 +14,6 @@ export async function findFreeRam(ns, neededRAM) {
 	}
 	const maxRam = ns.getServerMaxRam("home");
 	const ramUsed = ns.getServerUsedRam("home");
-	if (neededRAM <= maxRam - ramUsed) { return "home" }
-	return "unavailable";
+	if (neededRAM <= maxRam - ramUsed) { return 'home' }
+	return 'none';
 }
