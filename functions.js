b0f652d2be1b38eb99b141c0120637e9124135f4
OneSidedDie <41028043+OneSidedDie@users.noreply.github.com>
2024-10-04 10:01:23 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,19 @@
-
+/** @param {NS} ns */
+/** @param {number} amount - Amount of free RAM to find on any server that is available. */
+/** @returns {string} - Name of host with required free RAM or "none" if none available. */
+export async function findFreeRam(ns, neededRAM) {
+	const hosts = ns.read("hostsRAM.txt").split(",");
+	for (let i = 1, j = hosts.length; i < j; i++) {
+		const maxRam = ns.getServerMaxRam(hosts[i]);
+		const ramUsed = ns.getServerUsedRam(hosts[i]);
+		if (ns.hasRootAccess(hosts[i]) === true) {
+			if (neededRAM <= maxRam - ramUsed) {
+        return hosts[i];
+			}
+		}
+	}
+	const maxRam = ns.getServerMaxRam("home");
+	const ramUsed = ns.getServerUsedRam("home");
+	if (neededRAM <= maxRam - ramUsed) { return "home" }
+	return "unavailable";
+}
