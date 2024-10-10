f1bb5ba1bc89a110af71fea2fac21b8862ff1a1c
OneSidedDie <41028043+OneSidedDie@users.noreply.github.com>
2024-10-09 22:04:55 -0400

    Update batcher.js

--- second/batcher.js
+++ second/batcher.js
@@ -2,40 +2,48 @@ import { findAvailRam } from 'lib.js';
 
 /** @param {NS} ns */
 export async function main(ns) {
-  const debug = false; // Boolean set to true for verbose logging
+  const debug = false; //Set true to enable logs.
+  const target = ns.args[0];
   if (!debug) {
     ns.disableLog('ALL');
   } else {
     ns.tail();
+    ns.print(`Launcher, target: ${target}`);
+    ns.print(ns.args[0]);
+    ns.print(`Launcher, debug: ${debug}`);
   }
-  const lCost = 3; //Ram (GB) cost of 'launcher.js'
-  const target = ns.args[0]; // String name of server to hack
-  const hosts = ns.read('hostNames.txt').split(',');
-  if (hosts.indexOf(target) === -1) {
-    ns.tprint(`Batcher, Invalid hostname: ${target}`);
-  }
-  const buffer = 200; //ms between calls so they don't overlap
-  let oneTime = true;
+  const hThreads = 1;
+  const gThreads = 10;
+  const wThreads1 = 1;
+  const wThreads2 = 1;
+  const hRam = 1.7 * hThreads; //Ram (GB) needed to run hThreads worth of hack.js
+  const gRam = 1.75 * gThreads;//Ram (GB) needed to run gThreads worth of grow.js
+  const wRam1 = 1.75 * wThreads1;//Ram (GB) needed to run wThreads worth of weaken.js
+  const wRam2 = 1.75 * wThreads2;//Ram (GB) needed to run wThreads worth of weaken.js
+
+  const buffer = 300;
+
   while (true) {
-    const hTime = Math.floor(ns.getHackTime(target));
-    const gTime = hTime * 3.2; // Grow time is *always 3.2 times longer than hack time.
-    const wTime = hTime * 4; // Weaken time is *always 4 times longer than hack time.
-    // Insert function to determine total available RAM 'slots' across all servers for optimal thread usage.  For now use inefficient but easy 1/10/2 ratio for HGW.
-    const hThreads = 1;
-    const gThreads = 10;
-    const wThreads = 2;
-    const instructions = JSON.stringify([target, hTime, gTime, wTime, hThreads, gThreads, wThreads, buffer, debug]);
-    ns.exec('launcher.js', findAvailRam(ns, lCost), 1, instructions);
+    const playerObj = ns.getPlayer();
+    const serverObj = ns.getServer(target);
+    serverObj.hackDifficulty = serverObj.minDifficulty;
+    serverObj.moneyAvailable = serverObj.moneyMax;
+    const hTime = ns.formulas.hacking.hackTime(serverObj, playerObj);
+    const gTime = ns.formulas.hacking.growTime(serverObj, playerObj);
+    const wTime1 = ns.formulas.hacking.weakenTime(serverObj, playerObj);
+    const startTime = Date.now();
+    const endTime = startTime + wTime1;
+    const hEnd = endTime - buffer;
+    const gEnd = endTime + buffer;
+    const wEnd = endTime + buffer * 2;
+
     if (debug) {
-      ns.print(instructions);
     }
-    if (oneTime) {
-      ns.tprint(`Batcher: first hack on ${target} landing in ${((wTime - (buffer * 2)) / 1000)}s`);
-      ns.print(`Batcher: first hack on ${target} landing in ${((wTime - (buffer * 2)) / 1000)}s`);
-      oneTime = false;
-    } await ns.sleep(buffer * 3);
-    if (debug) {
-      ns.exit();
-    }
+    //script, findAvailRam(ns, neededRam), threads, target, oTime, endTime, debug
+    ns.exec('weaken.js', findAvailRam(ns, wRam1), wThreads1, target, wTime1, endTime, debug);
+    ns.exec('weaken.js', findAvailRam(ns, wRam2 * wThreads2), wThreads2, target, wTime1, wEnd, debug);
+    ns.exec('grow.js', findAvailRam(ns, gRam * gThreads), gThreads, target, gTime, gEnd, debug);
+    ns.exec('hack.js', findAvailRam(ns, hRam), hThreads, target, hTime, hEnd, debug);
+    await ns.sleep(buffer * 3);
   }
 }
