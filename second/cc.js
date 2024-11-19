b4e8c9fb66b9fe437be0d39f263d01501153a1a1
OneSidedDie <41028043+OneSidedDie@users.noreply.github.com>
2024-11-19 13:01:10 -0500

    Update cc.js

--- second/cc.js
+++ second/cc.js
@@ -3,7 +3,9 @@ export async function main(ns) {
   ns.tail();
   ns.disableLog('sleep', 'disableLog');
   const solved = ["Unique Paths in a Grid II", "Unique Paths in a Grid I", "Compression I: RLE Compression", "Spiralize Matrix", "Algorithmic Stock Trader I", "Algorithmic Stock Trader II", "Merge Overlapping Intervals", "Array Jumping Game", , "Array Jumping Game II", "Encryption I: Caesar Cipher", "Subarray with Maximum Sum", "Total Ways to Sum", "Find Largest Prime Factor", "Minimum Path Sum in a Triangle"];
+  const allCC =  ["Find Largest Prime Factor","Subarray with Maximum Sum","Total Ways to Sum","Total Ways to Sum II","Spiralize Matrix","Array Jumping Game","Array Jumping Game II","Merge Overlapping Intervals","Generate IP Addresses","Algorithmic Stock Trader I","Algorithmic Stock Trader II","Algorithmic Stock Trader III","Algorithmic Stock Trader IV","Minimum Path Sum in a Triangle","Unique Paths in a Grid I","Unique Paths in a Grid II","Shortest Path in a Grid","Sanitize Parentheses in Expression","Find All Valid Math Expressions","HammingCodes: Integer to Encoded Binary","HammingCodes: Encoded Binary to Integer","Proper 2-Coloring of a Graph","Compression I: RLE Compression","Compression II: LZ Decompression","Compression III: LZ Compression","Encryption I: Caesar Cipher","Encryption II: Vigenère Cipher"];
   const hosts = ns.read('hostNames.txt').split(',');
+  const nSolved = `${solved.length}/${allCC.length}`;
   while (true) {
     const contracts = lookForWork(ns, hosts);
     let result;
